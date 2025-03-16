e71a83f3581c720671205a02013a10d7a9a58e29
Lucas de Jesus <lucas.contato020@gmail.com>
2025-03-16 18:08:21 +0000

    Adicionada a movimentação do cavalo utilizando loops aninhados.

--- xadrez.c
+++ xadrez.c
@@ -24,13 +24,30 @@ int main() {
 
     printf("\n");
 
-    // Movimentação da Rainha (8 casas para a esquerda)  usando "do-while"
+    // Movimentação da Rainha (8 casas para a esquerda) usando "do-while"
     printf("Movimento da Rainha:\n");
     int k = 1;
     do {
         printf("Esquerda\n");
         k++;
     } while (k <= 8);
+
+    printf("\n");
+
+    // Movimentação do Cavalo (2 casas para baixo, 1 para a esquerda) usando "loops aninhados"
+    printf("Movimento do Cavalo:\n");
+    
+    // Loop para as 2 casas para baixo usando "for"
+    for (int a = 1; a <= 2; a++) {
+        printf("Baixo\n");
+    }
+
+    // Loop para 1 casa para a esquerda usando "while"
+    int b = 1;
+    while (b <= 1) {
+        printf("Esquerda\n");
+        b++;
+    }
 
     return 0;
 }
